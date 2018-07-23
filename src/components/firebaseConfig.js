20432cd96990b5f9baad382001b76ac56c8bf912
Shah Nawaz Shuvo <shahshuvo@yahoo.com>
2018-07-23 23:43:37 +0600

    Initial Commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyBz0_TrE6t-GAOgBO01cA-6sHYV3Wsqsu0",
+    authDomain: "vue-js-1824.firebaseapp.com",
+    databaseURL: "https://vue-js-1824.firebaseio.com",
+    projectId: "vue-js-1824",
+    storageBucket: "vue-js-1824.appspot.com",
+    messagingSenderId: "637437622451"
+}
